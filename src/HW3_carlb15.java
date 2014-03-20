2fbe574986635e5e6efe9b19711cea26f8f16a6a
Carl Barbee <carlb15@vt.edu>
2014-03-20 14:46:44 -0400

    Adding parsing for command line input.

--- src/HW3_carlb15.java
+++ src/HW3_carlb15.java
@@ -2,15 +2,28 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+// -------------------------------------------------------------------------
+/**
+ * @author carlbarbee
+ * @version Mar 20, 2014
+ */
 public class HW3_carlb15
 {
-    private static final int N            = 2;
-    private static final int THREAD_COUNT = 4;
+    private static int          N, THREAD_COUNT;
+    private static final String lockBasedQueue        = "LOCK_BASED";
+    private static final String concurrentLinkedQueue = "CONCURRENT_LINKED";
 
 
     // ----------------------------------------------------------
     /**
-     * Place a description of your method here.
+     * N-super-queue consists of N linearizable queues. Two types of
+     * N-super-queue are implemented: The subqueue is a regular sequential
+     * queue, protected against concurrent access by using a lock. Uses a native
+     * lock synchronized blocks/methods. The subqueue is a native concurrent
+     * queue (ConcurrentLinkedQueue). Measures the throughput (number of
+     * operations enqueue/dequeue per second) of each implementation and
+     * includes an excel sheet comparing the throughput as a function of the
+     * number of threads.
      *
      * @param args
      * @throws IllegalAccessException
@@ -28,22 +41,69 @@ public class HW3_carlb15
         IllegalArgumentException,
         InvocationTargetException
     {
-        // TODO Sub-Queue Type, Number of Sub-Queues, Number of Threads
-
-        final N_Super_Queue<Integer> superQueue =
-            new N_Super_Queue<Integer>(ConcurrentLinkedQueue.class, N);
-
-        Random rand = new Random();
-
-        for (int i = 0; i < 1000 * N; i++)
+        if (args.length == 3 && args[0].equals(lockBasedQueue)
+            && parseInt(args[1]) != null && parseInt(args[2]) != null)
         {
-            int value = rand.nextInt(N);
-            superQueue.enqueue(value);
+            final N_Super_Queue<Integer> superQueue;
+            N = parseInt(args[1]);
+            THREAD_COUNT = parseInt(args[2]);
+            superQueue = new N_Super_Queue<Integer>(LockBasedQueue.class, N);
+            Random rand = new Random();
+
+            for (int i = 0; i < 10 * N; i++)
+            {
+                int value = rand.nextInt(N);
+                superQueue.enqueue(value);
+            }
+
+            for (int i = 0; i < THREAD_COUNT; i++)
+            {
+                new TestThread(superQueue, THREAD_COUNT).start();
+            }
         }
 
-        for (int i = 0; i < THREAD_COUNT; i++)
+        else if (args.length == 3 && args[0].equals(concurrentLinkedQueue)
+            && parseInt(args[1]) != null && parseInt(args[2]) != null)
         {
-            new TestThread(superQueue, THREAD_COUNT).start();
+            final N_Super_Queue<Integer> superQueue;
+            N = parseInt(args[1]);
+            THREAD_COUNT = parseInt(args[2]);
+            superQueue =
+                new N_Super_Queue<Integer>(ConcurrentLinkedQueue.class, N);
+            Random rand = new Random();
+
+            for (int i = 0; i < 10 * N; i++)
+            {
+                int value = rand.nextInt(N);
+                superQueue.enqueue(value);
+            }
+
+            for (int i = 0; i < THREAD_COUNT; i++)
+            {
+                new TestThread(superQueue, THREAD_COUNT).start();
+            }
+        }
+
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Attempts to convert the string to an integer.
+     *
+     * @param text
+     *            The text to convert.
+     * @return text the string input to parse.
+     */
+    public static Integer parseInt(String text)
+    {
+        try
+        {
+            return new Integer(text);
+        }
+        catch (NumberFormatException e)
+        {
+            return null;
         }
     }
 }
